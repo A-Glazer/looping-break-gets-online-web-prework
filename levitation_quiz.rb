154183ffa6cad4a0296a7fddf030bf1b04e58dcf
A Glazer <avigayilglazer@gmail.com>
2018-12-26 13:05:10 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
   loop do
   	puts "What is the spell that enacts levitation?"
-	  answer = user
+	  answer = gets
   	break if answer == "Wingardium Leviosa"
 	end
 	  puts "You passed the quiz!"
